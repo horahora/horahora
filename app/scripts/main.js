ef99c2d47d0280486a81fc62eaad84e2f5024318
qiujunbin <qiujunbin@hhotel.com>
2020-06-24 00:38:46 +0800

    feat: 完善照片作品页的功能

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,36 +1,51 @@
-'use strict';
-if(typeof document.addEventListener == 'function') {
-  document.addEventListener('DOMContentLoaded', function() {
+"use strict";
+if (typeof document.addEventListener === "function") {
+  document.addEventListener(
+    "DOMContentLoaded",
+    function() {
+      var portfolio = document.getElementById("portfolio"),
+        hand = document.querySelector("#about-me .content");
 
-    var portfolio = document.getElementById('portfolio'),
-        hand = document.querySelector('#about-me .content');
+      var isTouch =
+        "ontouchstart" in window ||
+        (window.DocumentTouch && document instanceof DocumentTouch);
+      if (isTouch) {
+        document.documentElement.className += " touch";
+      }
 
-    var isTouch = ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
-    if(isTouch) {
-      document.documentElement.className += ' touch';
-    }
-
-    // 对不支持srcset属性的retina设备回退
-    if (window.devicePixelRatio >= 2 && document.createElement('img').srcset === undefined) {
-      var imgs = document.querySelectorAll('img');
-      for(var i = 0, len = imgs.length; i < len; i++) {
-        if(imgs[i].getAttribute('srcset') !== null) {
-          var retinaImg = imgs[i].getAttribute('srcset').match(/([^,\s]+@2x\.(?:png|jpg|gif))\s+2x/i)[1];
-          imgs[i].setAttribute('src', retinaImg)
+      // 对不支持srcset属性的retina设备回退
+      if (
+        window.devicePixelRatio >= 2 &&
+        document.createElement("img").srcset === undefined
+      ) {
+        var imgs = document.querySelectorAll("img");
+        for (var i = 0, len = imgs.length; i < len; i++) {
+          if (imgs[i].getAttribute("srcset") !== null) {
+            var retinaImg = imgs[i]
+              .getAttribute("srcset")
+              .match(/([^,\s]+@2x\.(?:png|jpg|gif))\s+2x/i)[1];
+            imgs[i].setAttribute("src", retinaImg);
+          }
         }
       }
-    }
 
-    // 滚动到一定程度显示手的滑动动画(考虑添加touchend事件)
-    hand.classList.add('transparent');
-    window.addEventListener('scroll', function() {
-      if(hand.getBoundingClientRect().top + hand.offsetHeight / 3 < window.innerHeight) {
-        hand.classList.add('hand-animation');
-      }
-    }, false)
+      // 滚动到一定程度显示手的滑动动画(考虑添加touchend事件)
+      hand.classList.add("transparent");
+      window.addEventListener(
+        "scroll",
+        function() {
+          if (
+            hand.getBoundingClientRect().top + hand.offsetHeight / 3 <
+            window.innerHeight
+          ) {
+            hand.classList.add("hand-animation");
+          }
+        },
+        false
+      );
 
-    // 去除hover状态对动画的干扰
-    /*
+      // 去除hover状态对动画的干扰
+      /*
 portfolio.addEventListener('webkitAnimationStart', function(event) {
       event.target.classList.add('disable-hover');
     }, false);
@@ -47,6 +62,7 @@ portfolio.addEventListener('webkitAnimationStart', function(event) {
       event.target.classList.add('animated');
     }, false);
 */
-
-  }, false)
+    },
+    false
+  );
 }
